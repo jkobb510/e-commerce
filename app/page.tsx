b7e934f4ee4a800cbe1fc828294733273036e7f3
Seung Lee <jkobb510@yahoo.com>
2023-10-22 02:03:08 -0400

    loginpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,83 +1,36 @@
+'use client';
 import Image from 'next/image'
+import React from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+  const router = useRouter();
+
+  const handleLogin = () => {
+    // Add your login logic here
+    router.push('/app/login.tsx');
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
-      <div className="grid text-center lg:max-w-2xl lg:xl lg:w-full lg:mb- lg:grid-cols-4 lg:text-center">
-        <a
-          href="/about"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`text-2xl font-semibold`}>
-            About{''}
-          </h2>
-        </a>
+    <div>
+    <div className="container">
+    <label htmlFor="uname"><b>Username</b></label>
+    <input type="text" placeholder="Username" name="uname" required />
 
-        <a
-          href="/shop.tsx"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`text-2xl font-semibold`}>
-            Shop{' '}
-          </h2>
-        </a>
+    <label htmlFor="psw"><b>Password</b></label>
+    <input type="password" placeholder="Password" name="psw" required />
 
-        <a
-          href="/cart.tsx"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`text-2xl font-semibold`}>
-            Cart{' '}
-          </h2>
-        </a>
-
-        <a
-          href="/contact.tsx"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`text-2xl font-semibold`}>
-            Contact{' '}
-          </h2>
-        </a>
+    <div className="flex-column">
+      <button type="submit">Login</button>
+      <div className="remember-me">
+        <input type="checkbox"  name="remember" />
+        <label htmlFor="remember">Remember me</label>
       </div>
-      <section className="featured-products">
-  <div className="product">
-    <div className="product-image image-container">
-      <img src="/table.png" alt="Product 1" />
     </div>
-    <h3>Table</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <span className="price block">$99.99</span>
-    <button>Add to Cart</button>
   </div>
 
-  <div className="product">
-    <div className="product-image image-container">
-      <img src="/coffee.png" alt="Product 2" />
-    </div>
-    <h3>Coffee Cup</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <span className="price block">$79.99</span>
-    <button>Add to Cart</button>
+  <div className="container" style={{ backgroundColor: '#f1f1f1' }}>
+    <span className="psw">Forgot <a href="#">password?</a></span>
   </div>
-  <div className="product">
-  <div className="product-image image-container">
-      <img src="/knife.png" alt="Product 2" />
-    </div>
-    <h3>Knife Set</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    <span className="price block">$79.99</span>
-    <button>Add to Cart</button>
-  </div>
-</section>
-    </main>
-  )
+</div>
+);
 }
