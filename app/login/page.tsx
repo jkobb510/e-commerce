b75a8e69c8587259394347a1cac8373350a82be7
Seung Lee <jkobb510@yahoo.com>
2023-10-31 15:04:45 -0400

    login page restore changes, cart page change

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,23 +6,40 @@ import 'app/login/page.css';
 import Link from 'next/link';
 export default function Login() {
   const router = useRouter();
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [isAuthorized, setIsAuthorized] = useState(false);
-
-  const handleLogin = () => {
-    console.log('Username:', username);
-    console.log('Password:', password);
-    const isAuthorized = username === 'john' && password === 'changeme';
-
-        setIsAuthorized(isAuthorized);
-
-    if (isAuthorized) {
+  const [loginData, setLoginData] = useState({
+    username: '',
+    password: ''
+  });
+  const handleLogin = async () => {
+    try {
+    const response = await fetch(process.env.LOCALSERVER_URL + '/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(loginData)
+    });
+      if (response.ok) {
+      const data = await response.json();
+      const { token } = data;
+      localStorage.setItem('token', token);
       router.push('/');
     } else {
+      // Handle login error
       console.error('Login failed');
     }
-    };
+  } catch (error) {
+    // Handle network or server error
+    console.error('An error occurred while logging in:', error);
+  }
+};
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setLoginData({
+      ...loginData,
+      [name]: value
+  })
+  };
   return (
     <div className="main-content">
     <main className="flex min-h-screen flex-col items-center justify-between p-0 ">
@@ -52,13 +69,23 @@ export default function Login() {
 
 
         <a
-          href="/contact.tsx"
+          href="/contact"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
           rel="noopener noreferrer"
         >
           <h4 className={`text-1xl`}>
-            Contact{' '}
+            Contact{''}
+          </h4>
+        </a>
+        <a
+          href="/cart"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h4 className={`text-1xl`}>
+            Cart{''}
           </h4>
         </a>
       </div>
@@ -69,19 +96,17 @@ export default function Login() {
       <input
         type="text"
         placeholder="Username"
-        name="uname"
-        required
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
+        name="username"
+        value={loginData.username}
+        onChange={handleInputChange}
       />
       
     <input
         type="password"
         placeholder="Password"
-        name="psw"
-        required
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        value={loginData.password}
+        onChange={handleInputChange}
       />
 
     <div className="flex-column">
@@ -89,18 +114,14 @@ export default function Login() {
         <input className='space' type="checkbox"  name="remember" />
         <label htmlFor="remember">Remember me</label>
       </div>
+
       <button className="login-button" type="submit" onClick={handleLogin}>
         Login
       </button>
-      <button
-        className="login-button"
-        type="submit"
-        onClick={() => router.push('/register')}
-      >
+
+      <button className="login-button" type="submit" onClick={() => router.push('/register')}>
         Register
       </button>
-
-
     </div>
   <div className="container" style={{ backgroundColor: '#f1f1f1' }}>
     <span className="psw">Forgot <a href="#">password?</a></span>
