a2ac68a131246084de50e6c2c2e5a6906b0c7260
jkobb510 <jkobb510@yahoo.com>
2023-11-13 20:50:52 -0500

    welcome title

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -49,7 +49,7 @@ export default function About() {
             <section className="px-4 py-8 text-center">
             <img src="/e-commercelogo.png" alt="Logo" className="logo" />
 
-        <h1 className="text-3xl font-semibold mb-4">Welcome to our e-commerce store!</h1>
+        <h2 className="text-3xl font-semibold mb-4">Welcome to our e-commerce store!</h2>
         <p className="text-lg m-10">
           At our e-commerce store, we strive to provide you with the best shopping experience. We offer a wide range of products, including electronics, fashion, home decor, and more. With our user-friendly website and secure payment options, you can shop with confidence. Our dedicated customer support team is always ready to assist you with any queries or concerns. We value your satisfaction and aim to deliver high-quality products and exceptional service. Start exploring our store today and find amazing deals on your favorite items!
         </p>
